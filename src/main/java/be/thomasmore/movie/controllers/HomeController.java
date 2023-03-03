2e2bfd4b0f34954f51e803c9d79a8831cdb72278
ahmetnuriuygun <ahmetnuriuygun1@gmail.com>
2023-03-03 16:55:18 +0100

    navbar and footer has toegevoegd

--- src/main/java/be/thomasmore/movie/controllers/HomeController.java
+++ src/main/java/be/thomasmore/movie/controllers/HomeController.java
@@ -1,2 +1,12 @@
-package be.thomasmore.movie.controllers;public class HomeController {
+package be.thomasmore.movie.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
