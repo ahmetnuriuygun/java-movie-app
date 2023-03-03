26575ad380a6109de92f85df1f38a7ae9cdcdbbc
ahmetnuriuygun <ahmetnuriuygun1@gmail.com>
2023-03-03 09:34:40 +0100

    Project has created

---
+++ src/test/java/be/thomasmore/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
